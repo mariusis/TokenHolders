f2631f7125f5f812328f46d8c1ec55589ddc85e4
mariusis <spider_child2001@yahoo.com>
2024-08-29 16:10:06 +0300

    Test Commit --Ignore

--- src/components/TokenInfoDisplay.tsx
+++ src/components/TokenInfoDisplay.tsx
@@ -37,7 +37,7 @@ const TokenInfoDisplay = () => {
               className="w-fit p-2 mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
               type="submit"
             >
-              Sepolia Etherscan
+              Sepolia Etherscan Link
             </button>
           </a>
         </div>
