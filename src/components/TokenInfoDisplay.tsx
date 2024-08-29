7944148401ca4955eebc23a21d43c8d0fc261072
mariusis <spider_child2001@yahoo.com>
2024-08-29 13:54:31 +0300

    Refactored wallet address and contract links to Etherscan Sepolia. Added button to easily navigate to contract page

--- src/components/TokenInfoDisplay.tsx
+++ src/components/TokenInfoDisplay.tsx
@@ -28,6 +28,20 @@ const TokenInfoDisplay = () => {
   return (
     <div>
       <div>
+        <div>
+          <h1> Contract Address: </h1>
+          <a
+            href={`https://sepolia.etherscan.io/address/0xb1EA3b0211bee07388937Ae6Bdf2537c62DD6B92`}
+          >
+            <button
+              className="w-fit p-2 mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+              type="submit"
+            >
+              Sepolia Etherscan
+            </button>
+          </a>
+        </div>
+
         <div className="text-3xl   w-fit mx-auto my-3">
           <p>Total number of holders:</p>
           <p>{tokenHolders}</p>
@@ -44,7 +58,14 @@ const TokenInfoDisplay = () => {
           <tbody>
             {wallets.map((wallet, index) => (
               <tr key={index}>
-                <td className="border border-gray-400 p-2">{wallet[0]}</td>
+                <td className="border border-gray-400 p-2 hover:text-blue-500">
+                  <a
+                    href={`https://sepolia.etherscan.io/address/${wallet[0]}`}
+                    target="_blank"
+                  >
+                    {wallet[0]}
+                  </a>
+                </td>
                 <td className="border border-gray-400 p-2">
                   Balance: {formatUnits(wallet[1], 18)}
                 </td>
