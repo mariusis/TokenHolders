3abd82a900a0b7887e1b88f73ba8cb74a35e1055
mariusis <spider_child2001@yahoo.com>
2024-08-29 15:06:17 +0300

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  base: "/TokenHolders/", // Important: Set the base URL to match your GitHub Pages URL
+});
