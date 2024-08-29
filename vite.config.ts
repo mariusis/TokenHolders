75b6ba9b1834ee25579c1e6b10d3afb5af2e9088
mariusis <spider_child2001@yahoo.com>
2024-08-29 16:26:08 +0300

    updated build path(+cleaned unused scripts)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "docs",
   },
   base: "/TokenHolders/", // Important: Set the base URL to match your GitHub Pages URL
 });
