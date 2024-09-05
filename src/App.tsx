2e3932f2c43090b255a3a841ad310ae66aec0d97
mariusis <spider_child2001@yahoo.com>
2024-09-05 18:49:55 +0300

    Added a navbar and adjusted the app routing + some small css tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import BalanceCheckForm from "./components/BalanceCheckForm";
+import HolderWalletsDisplay from "./components/HolderWalletsDisplay";
 
 function App() {
   return (
-    <div className="text-center">
+    <div>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/TokenHolders" element={<Home />} />
+          <Route
+            path="/TokenHolders/balanceCheck"
+            element={<BalanceCheckForm />}
+          />
+          <Route
+            path="/TokenHolders/holderWallets"
+            element={<HolderWalletsDisplay />}
+          />
         </Routes>
       </Router>
     </div>
