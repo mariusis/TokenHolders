2a524366d1d2fff2aedd0677889c2f478d2ee65a
mariusis <spider_child2001@yahoo.com>
2024-09-09 14:41:33 +0300

    Added Error Boundaries to main app routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,37 @@ import Navbar from "./components/Navbar";
 import BalanceCheckForm from "./components/BalanceCheckForm";
 import HolderWalletsDisplay from "./components/HolderWalletsDisplay";
 import Footer from "./components/Footer";
+import ErrorBoundary from "./utils/ErrorBoundary";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen flex-grow ">
+    <div className="flex flex-col min-h-screen flex-grow">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/TokenHolders" element={<Home />} />
+          <Route
+            path="/TokenHolders"
+            element={
+              <ErrorBoundary>
+                <Home />
+              </ErrorBoundary>
+            }
+          />
           <Route
             path="/TokenHolders/balanceCheck"
-            element={<BalanceCheckForm />}
+            element={
+              <ErrorBoundary>
+                <BalanceCheckForm />
+              </ErrorBoundary>
+            }
           />
           <Route
             path="/TokenHolders/holderWallets"
-            element={<HolderWalletsDisplay />}
+            element={
+              <ErrorBoundary>
+                <HolderWalletsDisplay />
+              </ErrorBoundary>
+            }
           />
         </Routes>
         <Footer />
